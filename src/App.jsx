3a647d9e50cc966d7ea617e0f6bb4560cb6e1f8c
arjun <arjunshekhawatarjun@gmail.com>
2024-01-09 21:43:44 +0530

    Add initial rendering of activity list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import activityService from '../services/activity'
+import ActivityList from '../components/ActivityList'
 
 function App() {
+  const [activities, setActivities] = useState([])
+
+  useEffect(() => {
+    console.log('App.js useEffect()')
+    getActivities()
+  }, [])
+
+
+  const getActivities = () => {
+    activityService
+      .getAllActivities()
+      .then(allActivities => {
+        console.log(allActivities)
+        setActivities(allActivities)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
   return (
     <div>
       Hello World!
+      <ActivityList activities={activities} />
     </div>
   )
 }
